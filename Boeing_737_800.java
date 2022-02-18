2967d4d958958a426e5e8480b20520f518cb79b5
nauralibna <99931028+nauralibna@users.noreply.github.com>
2022-02-18 08:31:14 +0700

    full

--- Boeing_737_800.java
+++ Boeing_737_800.java
@@ -1,45 +1,23 @@
-// __  __            __           ___                     
-///\ \/\ \          /\ \      __ /\_ \                    
-//\ \ `\\ \     __  \ \ \____/\_\\//\ \     ____    ____  
-// \ \ , ` \  /'__`\ \ \ '__`\/\ \ \ \ \   /',__\  /',__\ 
-//  \ \ \`\ \/\ \L\.\_\ \ \L\ \ \ \ \_\ \_/\__, `\/\__, `\
-//   \ \_\ \_\ \__/.\_\\ \_,__/\ \_\/\____\/\____/\/\____/
-//    \/_/\/_/\/__/\/_/ \/___/  \/_/\/____/\/___/  \/___/ 
 
-///////////////////////////////////
-//Author : Nabil Sahsada Suratno///
-//Mode : Java Main Class        ///
-//Youtube : NabilGames          ///
-///////////////////////////////////
+package pewarisanpesawat;
 
-package Pesawat;
+public class Boeing_737_800 extends Pesawat {
 
-
-public class Boeing_737_800 extends Pesawat{
-Pesawat pesawat = new Pesawat();
-private int kecepatanPesawat,kecepatanUdara;
-private String fasilitas;
-
-
-
-
-//Airbus A302 Series    
-    public void Boeing_737_800(){
+   public void Boeing_737_800(){
       pesawat.setJenisPesawat("BOIENG 737-800");
       pesawat.setMesin(2);
       pesawat.setJenisMesin("Jet");
       pesawat.setJmlhPenumpang(180);
     }
-    
-      public void Boeing_737_800(int kecepatanUdara){
-        this.kecepatanUdara = kecepatanUdara;
-           
-    }
-    
     public void kelebihan(int kecepatan, String fasilitas){
         this.kecepatanPesawat = kecepatan;
         this.fasilitas = fasilitas;        
     }
+   public void Boeing_737_800(){
+      pesawat.setJenisPesawat("BOIENG 737-800");
+      pesawat.setMesin(2);
+      pesawat.setJenisMesin("Jet");
+    
    
     public void get(){
         System.out.println("=============================================="+pesawat.getJenisPesawat()+"================================================");
@@ -47,8 +25,7 @@ private String fasilitas;
         System.out.println("Pesawat Ini memiliki \t\t= "+pesawat.getMesin()+" Mesin");
         System.out.println("Pesawat Ini Memilki Jenis Mesin \t= "+super.jenisMesin);
         System.out.println("Pesawat Ini memiliki kursi sejumlah \t= "+pesawat.getJmlhPenumpang());
-        System.out.println("Pesawat Ini memiliki kecepatan diudara adalah \t= "+this.kecepatanUdara);
-        System.out.println("Pesawat Ini memiliki kecepetan didarat adalah \t= "+this.kecepatanPesawat);
+        System.out.println("Pesawat Ini memiliki kecepatan diudara adalah \t= "+this.kecepatanPesawat);
         System.out.println("Pesawat Ini memiliki fasilitas yaitu \t= "+this.fasilitas);
         System.out.println("==============================================================================================");
     }
@@ -57,3 +34,9 @@ private String fasilitas;
 
     
 }
+    
+
+
+    
+}
+
---
+++ PewarisanPesawat.java
@@ -0,0 +1,51 @@
+
+package pewarisanpesawat;
+
+  public class Pesawat {
+    // Buat Kodingan Neng Kene Bro OKE:D
+   
+String jenisMesin = "JET",jenisPesawat;
+int mesin = 0, jmlhPenumpang = 0;
+
+    public void setJenisMesin(String jenisMesin) {
+        this.jenisMesin = jenisMesin;
+    }
+
+    public void setMesin(int mesin) {
+        this.mesin = mesin;
+    }
+
+    public void setJmlhPenumpang(int jmlhPenumpang) {
+        this.jmlhPenumpang = jmlhPenumpang;
+    }
+
+    public void setJenisPesawat(String jenisPesawat) {
+        this.jenisPesawat = jenisPesawat;
+    }
+    
+    
+    public String getJenisMesin() {
+        return jenisMesin;
+    }
+
+    public int getMesin() {
+        return mesin;
+    }
+
+    public int getJmlhPenumpang() {
+        return jmlhPenumpang;
+    }
+
+    public String getJenisPesawat() {
+        return jenisPesawat;
+    }
+
+    
+
+
+
+   
+   
+  
+  
+}
